ed3a8c3f123e4dd62eb3a46e6c2c1545bc8b0ebe
Rob Savoye <rob@senecass.com>
2020-05-21 10:44:30 -0600

    Populate the manifest data from the database

--- contrib/database/manifest.py
+++ contrib/database/manifest.py
@@ -100,22 +100,8 @@ class AbeManifest(object):
                     print("ERROR: Query that failed: %r" % query)
                     quit()
 
-    def queryManifest(self):
-        pass
-        # query = "SELECT branch,filespec,revison.md5sum FROM manifest WHERE testrun=%r AND tool='gcc'" % (testrun)
-        # try:
-        #     self.post.execute(query)
-        # except Exception as e:
-        #     if e.pgcode != None:
-        #         print("ERROR: Query failed to fetch! %r" % e.pgerror)
-        #         quit()
-        # tmp = self.post.fetchone()
-        # if tmp:
-        #     self.branch = tmp[0]
-        #     self.filespec = tmp[1]
-        # else:
-        #     self.branch = None
-        #     self.filespec = None
+    def get(self, tool, key):
+        return self.manifest[tool][key]
 
     def dump(self):
         for tool,entry in self.manifest.items():
@@ -123,4 +109,26 @@ class AbeManifest(object):
                 if len(entry) > 1:
                     print("Details for %s" % tool)
                     for key,value in entry.items():
-                        print("\t%s_%s = %s" % (tool, key, value))
+                        if value:
+                            print("\t%s_%s = %s" % (tool, key, value))
+
+    def populate(self, testrun):
+        query = """SELECT tool,branch,filespec,revision,md5sum FROM manifest WHERE testrun=%r;""" % testrun
+        try:
+            self.post.execute(query)
+        except Exception as e:
+            if e.pgcode != None:
+                print("ERROR: Query failed to fetch! %r" % e.pgerror)
+                print("ERROR: Query that failed: %r" % query)
+                quit()
+
+        result = self.post.fetchall()
+        for entry in result:
+            data = dict()
+            print(entry)
+            tool = entry[0]
+            data['branch'] = entry[1]
+            data['filespec'] = entry[2]
+            data['revision'] = entry[3]
+            data['md5sum'] = entry[4]
+            self.manifest[tool] = data
